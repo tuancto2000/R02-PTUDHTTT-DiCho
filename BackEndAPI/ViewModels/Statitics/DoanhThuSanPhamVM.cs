b73160cb84c372cc8739dbd26b533a36bb55f148
Nguyen Xuan Tuan <58553278+tuancto2000@users.noreply.github.com>
2022-01-23 12:06:01 +0700

    add statitic vm

---
+++ BackEndAPI/ViewModels/Statitics/DoanhThuSanPhamVM.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BackEndAPI.ViewModels.Statitics
+{
+    public class DoanhThuSanPhamVM
+    {
+        public string Ten { get; set; }
+        public int SoLuongBanRa { get; set; }
+        public int DoanhThu { get; set; }
+    }
+}
